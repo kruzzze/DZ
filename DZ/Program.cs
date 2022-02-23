a8d978435e5b9560364729896600dae1e179c2f7
FosseyCool <Verein33@gmail.com>
2022-02-23 20:40:23 +0300

    DZ1

---
+++ DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DZ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rest = new Restaurant();
+
+            var communication = new Communication();
+            communication.Dialog();
+            communication.RemoveAllBooking();
+        }
+    }
+}
---
+++ DZ/State.cs
@@ -0,0 +1,16 @@
+﻿namespace DZ
+{
+    public enum State
+    {
+        /// <summary>
+        /// Стол свободен
+        /// </summary>
+        Free=0,
+            
+        /// <summary>
+        /// стол занят
+        /// </summary>
+        Booked=1
+
+    }
+}
